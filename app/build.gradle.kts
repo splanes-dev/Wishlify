4d37cb8136a30b88e71ad561e55ca29f7280ce12
splanes <splanes@protonmail.com>
2026-05-03 14:21:24 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.splanes.uoc.wishlify"
     minSdk = 30
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 110
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
